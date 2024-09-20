20845f629e62590de5abcf8b54bbcb4c4273631f
devdivyanshu1 <154589654+devdivyanshu1@users.noreply.github.com>
2024-09-21 00:11:06 +0530

    Update hi.cpp

--- hi.cpp
+++ hi.cpp
@@ -1,57 +1,46 @@
 #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+// best on dp on trees;
+int aa = -1e9;
 
-int ann = 0;
-
-void dfs(int node, vector<int> adj[], int par, vector<int> &ans)
-{
-    ans[node] = 0;
-    int max1 = 0, max2 = 0;
-
-    for (auto it : adj[node])
-    {
-        if (it != par)
-        {
-            dfs(it, adj, node, ans);
-            int curr = ans[it] + 1;
-            ans[node] = max(ans[node], curr);
-
-            if (curr >= max1)
-            {
-                max2 = max1;
-                max1 = curr;
-            }
-            else if (curr > max2)
-            {
-                max2 = curr;
+void dfs(int node, int par, vector<int> adj[], vector<int>& ans) {
+    for (auto it : adj[node]) {
+        if (it != par) {
+            dfs(it, node, adj, ans);
+        }
+    }
+    
+    int h1 = 0, h2 = 0;
+    for (auto tt : adj[node]) {
+        if (tt != par) {
+            if (h1 <= 1 + ans[tt]) {
+                h2 = h1;
+                h1 = ans[tt] + 1;
+            } else {
+                h2 = max(h2, ans[tt] + 1);
             }
         }
     }
-
-    int dia_node = max1 + max2;
-    ann = max(ann, dia_node);
+    aa = max(aa, h1 + h2);
+    ans[node] = h1;
 }
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
-
-    vector<int> adj[n + 1];
-
-    
-    for (int i = 0; i < n - 1; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
     vector<int> ans(n + 1, 0);
+    vector<int> adj[n + 1];
+    
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    
+    dfs(1, -1, adj, ans);
+    cout << aa << endl;
 
-    dfs(1, adj, -1, ans);
-
-    cout << ann << endl;
     return 0;
 }
